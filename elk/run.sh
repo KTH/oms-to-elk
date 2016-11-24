2b407554e727e58da92e37fd656b4ff9e6991474
Fredrik Jönsson <fjo@kth.se>
2016-11-24 15:21:54 +0100

    Better logstash config.

--- elk/run.sh
+++ elk/run.sh
@@ -2,14 +2,13 @@
 #
 # Helper script to create docker container with ELK stack.
 #
-dir=${0%/*}
 
 docker run \
   -p 5601:5601 \
   -p 9200:9200 \
   -p 5044:5044 \
   -p5000:5000 \
-  -v ${dir}/01-lumberjack-input.conf:/etc/logstash/conf.d/01-lumberjack-input.conf \
+  -v .../conf.d:/etc/logstash/conf.d \
   -it \
   --name elk \
   sebp/elk:es241_l240_k461
