174b0bf6f256bcb4ac8f6499a863c44ef5a68e9c
Fredrik Jönsson <fjo@kth.se>
2016-11-29 07:16:26 +0100

    Improve helper script.

--- elk/run.sh
+++ elk/run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 #
 # Helper script to create docker container with ELK stack.
+# Has to be run from current directory, ./run.sh
 #
 
-docker run \
-  -p 5601:5601 \
-  -p 9200:9200 \
-  -p 5044:5044 \
-  -p5000:5000 \
-  -v .../conf.d:/etc/logstash/conf.d \
-  -it \
-  --name elk \
-  sebp/elk:es241_l240_k461
+image=sebp/elk:es241_l240_k461
+
+if docker ps -a -f name=elk | grep -q $image; then
+  docker start -a elk
+else
+  docker run \
+    -p 5601:5601 \
+    -p 9200:9200 \
+    -p 5044:5044 \
+    -p5000:5000 \
+    -v `pwd`/conf.d:/etc/logstash/conf.d \
+    -it \
+    --name elk \
+    $image
+fi
