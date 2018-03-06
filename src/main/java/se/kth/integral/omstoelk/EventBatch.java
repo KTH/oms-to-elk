0241357d1f5ea06f03f8eb3f77e4ddd7a6b04a6e
Fredrik Jönsson <fjo@kth.se>
2018-03-06 20:39:50 +0100

    Make it compile but won't work as is.

--- src/main/java/se/kth/integral/omstoelk/EventBatch.java
+++ src/main/java/se/kth/integral/omstoelk/EventBatch.java
@@ -1,17 +1,17 @@
 package se.kth.integral.omstoelk;
 
 import java.io.UnsupportedEncodingException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
 
 import se.kth.infosys.lumberjack.Event;
 
@@ -20,20 +20,20 @@ public class EventBatch {
     private static final ObjectMapper OM = new ObjectMapper();
 
     private List<Event> events;
-    private DateTime timestamp;
+    private Instant timestamp;
 
-    public static EventBatch readEvents(final int batchSize, final Queue<JsonNode> queue) {
+    public static EventBatch readEvents(final int batchSize, final Queue<JsonObject> queue) {
         EventBatch batch = new EventBatch();
         batch.events = new ArrayList<Event>(batchSize);
 
         for (int i = 0; i < batchSize; i++) {
             try {
-                JsonNode json = queue.poll();
+                JsonObject json = queue.poll();
                 if (json == null) {
                     break;
                 }
                 batch.events.add(new Event().addField("line", OM.writeValueAsString(json)));
-                batch.timestamp = DateTime.parse(json.get("TimeGenerated").asText());
+                batch.timestamp = Instant.parse(json.get("TimeGenerated").getAsString());
             } catch (UnsupportedEncodingException | JsonProcessingException e) {
                 LOG.error("Failed to encode json: {}", e.getMessage());
             }
@@ -45,7 +45,7 @@ public class EventBatch {
         return events;
     }
 
-    public DateTime getTimestamp() {
+    public Instant getTimestamp() {
         return timestamp;
     }
 }
